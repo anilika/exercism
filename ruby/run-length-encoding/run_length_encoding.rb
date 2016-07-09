01731de33980c8846af0afab7cffefd4274de9ed
anilika <cute.red.box@gmail.com>
2016-07-09 22:05:14 +1000

    renamed amount war to number

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,12 +1,12 @@
 module RunLengthEncoding
   def self.encode(data)
-    amount, result = 0, ''
+    number, result = 0, ''
     data.chars.each_with_index do |char, index|
-      amount += 1
+      number += 1
       unless char == data[index+1]
-        amount = '' if amount == 1
-        result << amount.to_s + char
-        amount = 0
+        number = '' if number == 1
+        result << number.to_s + char
+        number = 0
       end
     end
     result
