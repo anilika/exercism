bf6986f7734b7d1de58c970191ac2207e9e594af
anilika <cute.red.box@gmail.com>
2016-07-10 16:39:11 +1000

    First version of sum-of-multiples

---
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,14 @@
+class SumOfMultiples
+  def initialize(*args)
+    @dividers = *args
+  end
+
+  def to(end_num)
+    (0...end_num).select do |char|
+      dividers.any?{|divider| (char % divider).zero? }
+    end.inject(:+)
+  end
+
+  private
+  attr_reader :dividers
+end
