cc0b8d703ff189b4d839e6397f340d64fd6691f5
anilika <cute.red.box@gmail.com>
2016-07-14 14:49:36 +1000

    First version of space_age

---
+++ ruby/space-age/space_age.rb
@@ -0,0 +1,20 @@
+class SpaceAge
+  YEAR_ON_PLANETS = {on_mercury: 0.2408467, on_venus: 0.61519726, on_mars: 1.8808158,
+                     on_jupiter: 11.862615, on_saturn: 29.447498, on_uranus: 84.016846,
+                     on_neptune: 164.79132}
+  EARTH_YEAR = 31557600.0
+
+  attr_reader :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  def method_missing(method)
+    seconds / (EARTH_YEAR * YEAR_ON_PLANETS[method])
+  end
+
+  def on_earth
+    seconds / EARTH_YEAR
+  end
+end
