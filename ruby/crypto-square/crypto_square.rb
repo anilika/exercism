8fc3e626ce7020b13fd9ad28fb393106078284c0
anilika <cute.red.box@gmail.com>
2016-07-20 14:34:24 +1000

    Changed all methods in crypto_square

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,46 +1,46 @@
 class Crypto
-  def initialize(phrase)
-    @phrase = normalize(phrase)
-    @columns_num = calc_size(@phrase)
-    @rows_num = (@phrase.size % @columns_num).zero? ? @phrase.size / @columns_num : @columns_num - 1
-  end
-
-  def normalize(phrase)
-    phrase.gsub(/\W+/, '').downcase
-  end
-
-  def plaintext_segments
-    @phrase.chars.each_slice(@columns_num).map {|chars| chars.join }
-  end
-
-  def ciphertext
-    crypt_segments = plaintext_segments.map { |chars| chars.chars }
-    crypt_segments[0].zip(*crypt_segments[1..-1]).flatten.join
-  end
-
-  def normalize_ciphertext
-    case @phrase.size - (@columns_num * @rows_num)
-      when 0
-       ciphertext.chars.each_slice(@rows_num).map { |chars| chars.join }.join(' ')
-      else
-        @rows_num += 1 if @rows_num == 1
-        phrase = ciphertext.chars.each_slice(@rows_num).map { |chars| chars.join }
-        num_little_pis = phrase[0].size - phrase[-1].size
-        end_phrase = phrase[0...-num_little_pis] + phrase[-num_little_pis..-1].join.chars.each_slice(@rows_num - 1).map{|x| x.join}
-        end_phrase.join(' ')
-     end
+  def initialize(plaintext)
+    @plaintext = plaintext
   end
 
   def normalize_plaintext
-    @phrase
-  end
-
-  def calc_size(length_phrase)
-    root = Math.sqrt(@phrase.size)
-    root.to_i == root ? root.to_i : root.to_i + 1
+    @plaintext.gsub(/\W+/, '').downcase
   end
 
   def size
-    @columns_num
+    @size ? @size : @size = calc_size
+  end
+
+  def plaintext_segments
+    separate(normalize_plaintext, size).map { |segment| segment.join }
+  end
+
+  def ciphertext
+    segments = separate(normalize_plaintext, size)
+    segments[0].zip(*segments[1..-1]).join
+  end
+
+  def normalize_ciphertext
+    rows_num = size == 2 ? size : size - 1
+    result = separate(ciphertext, rows_num).map { |segment| segment.join }
+    if normalize_plaintext.length == size * rows_num
+      result.join(' ')
+    else
+      num_spaces = result.first.size - result.last.size
+      ends_segments = separate(result[-num_spaces..-1].join, rows_num - 1)
+      (result[0...-num_spaces] + ends_segments.map {|segment| segment.join }).join(' ')
+    end
+  end
+
+  private
+
+  def separate(text, num)
+    return [text] if num == 0
+    text.chars.each_slice(num).to_a
+  end
+
+  def calc_size
+    root = Math.sqrt(normalize_plaintext.length)
+    root.to_i == root ? root.to_i : root.to_i + 1
   end
 end
