4ec6cc8fc41a97edb4c111891b86c4ff8ff62f53
anilika <cute.red.box@gmail.com>
2016-07-26 00:12:04 +1000

    Cleared code

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,5 +1,6 @@
 class Hexadecimal
-  LITERALS = {'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15 }
+  LITERALS = { 'a' => 10, 'b' => 11, 'c' => 12,
+               'd' => 13, 'e' => 14, 'f' => 15 }.freeze
 
   def initialize(hex_num)
     @hex_num = hex_num
@@ -7,7 +8,7 @@ class Hexadecimal
 
   def to_decimal
     return 0 unless @hex_num.scan(/\H/).empty?
-    @hex_num.chars.reverse.each_with_index.inject(0) do |sum , (num, index)|
+    @hex_num.chars.reverse.each_with_index.inject(0) do |sum, (num, index)|
       num = LITERALS[num] ? LITERALS[num] : num.to_i
       sum + num * (16**index)
     end
