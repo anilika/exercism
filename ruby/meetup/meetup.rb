c727cfa5a763ddfb2738b4d322dcbb984081e44c
anilika <cute.red.box@gmail.com>
2016-07-30 01:19:03 +1000

    First version of meetup

---
+++ ruby/meetup/meetup.rb
@@ -0,0 +1,27 @@
+require 'date'
+
+class Array
+  def second; self[1]; end
+
+  def third; self[2]; end
+
+  def fourth; self[3]; end
+end
+
+class Meetup
+  def initialize(month, year)
+    @first_day = Date.new(year, month)
+  end
+
+  def day(weekday, schedule)
+    month = @first_day.upto(@first_day.next_month - 1)
+    days = month.select { |day| day.send(weekday.to_s + '?') }
+    days.respond_to?(schedule) ? days.send(schedule) : teenth(days)
+  end
+
+  private
+
+  def teenth(days)
+    days.select { |day| (13..19).cover?(day.day) }[0]
+  end
+end
