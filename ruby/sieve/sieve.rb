0f4bdce4e000cb0dc482b64779d574918a18879d
anilika <cute.red.box@gmail.com>
2016-08-02 14:16:40 +1000

    Calculated prime numbers was serialized

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,15 +1,33 @@
 class Sieve
+  PRIMES_FILE = '/tmp/primes.data'.freeze
+  FIRST_PRIMES = [2, 3].freeze
+  STEP = 2
+
   def initialize(num)
     @num = num
+    @primes = load_primes
   end
 
   def primes
-    result = []
-    numbers = (2..@num).to_a
-    until numbers.empty?
-      result << numbers.shift
-      numbers.reject! { |num| (num % result.last).zero? }
+    return @primes.select { |prime| prime <= @num } if @primes.last >= @num
+    ((@primes.last + STEP)..@num).reject do |prime|
+      @primes << prime unless @primes.any? { |num| prime % num == 0 }
     end
-    result
+    dump_primes
+    @primes
+  end
+
+  private
+
+  def load_primes
+    begin
+      Marshal.load(File.read(PRIMES_FILE))
+    rescue Errno::ENOENT, ArgumentError
+      FIRST_PRIMES
+    end
+  end
+
+  def dump_primes
+    File.write(PRIMES_FILE, Marshal.dump(@primes))
   end
 end
