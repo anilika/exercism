c2c958c9babb7fb7168a5d295504f04db0d43096
anilika <cute.red.box@gmail.com>
2016-08-09 21:46:10 +1000

    Iteration was moved to separated method.

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -8,25 +8,27 @@ class Cipher
   end
 
   def encode(plaintext)
-    plaintext.chars.zip(key.chars).each_with_object('') do |pair, encode_text|
-      letter, key = pair
-      index_encode_letter = @abc.index(letter) + @abc.index(key)
-      encode_letter  = @abc[index_encode_letter] ? @abc[index_encode_letter] : @abc[index_encode_letter - 26]
-      encode_text << encode_letter
+    each_with_key(plaintext) do |letter, key|
+      index_letter = @abc.index(letter) + @abc.index(key)
+      @abc[index_letter] ? @abc[index_letter] : @abc[index_letter - @abc.size]
     end
   end
 
   def decode(ciphertext)
-    ciphertext.chars.zip(@key.chars).each_with_object('') do |pair, decode_text|
-      cipherletter, key = pair
-      index_decode_letter = @abc.index(cipherletter) - @abc.index(key)
-      decode_text << @abc[index_decode_letter]
+    each_with_key(ciphertext) do |cipherletter, key|
+      @abc[@abc.index(cipherletter) - @abc.index(key)]
     end
   end
 
   private
 
+  def each_with_key(text)
+    text.chars.zip(key.chars).map do |pair|
+      yield(pair.first, pair.last)
+    end.join
+  end
+
   def generated_key
-    (('a'..'z').to_a.reverse * 4).reverse.join
+    (0...50).map { ('a'..'z').to_a[rand(26)] }.join
   end
 end
